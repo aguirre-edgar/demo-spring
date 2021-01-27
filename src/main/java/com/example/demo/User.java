65b8cc4d10032483fae1828817bbd22ecc9a8298
Edgar <aguirre.edgar567@gmail.com>
2021-01-26 23:18:35 -0600

    small change

--- src/main/java/com/example/demo/User.java
+++
@@ -1,9 +0,0 @@
-
-@Entity
-public class Users {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private final String name;
-    private final String email;
-}
